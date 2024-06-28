10e4c98ef09f163264aace9aa5daeb7097d2c018
Harshit Mourya <mouryaharshit03@gmail.com>
2024-06-28 15:56:56 +0530

    deleted key

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -3,8 +3,7 @@ import search_icon from "./assets/search.png";
 import "./SearchBox.css";
 
 export default function SearchBox() {
-  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-  const API_KEY = "c2e16176807270857b580003f56bc373";
+  
   let [city, setCity] = useState("");
   let handleChange = (evt) => {
     setCity(evt.target.value);
